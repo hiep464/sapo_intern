12223ec22185381e187e21c5067e8af733c6112c
hiep464 <luuhiep2309@gmail.com>
2023-03-27 14:24:28 +0700

    fix: transaction

--- Ex7_Restful API_Spring/spring-boot-api/src/main/java/com/sapo/edu/demo/repository/ProductRepository.java
+++ Ex7_Restful API_Spring/spring-boot-api/src/main/java/com/sapo/edu/demo/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.sapo.edu.demo.repository;
 
 import com.sapo.edu.demo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-    @Query("delete from Product where categoryId = ?1")
-    public List<Product> deleteAllByCategoryId(Integer CategoryId);
+
+    public List<Product> deleteAllByCategoryId(Integer id);
 }
